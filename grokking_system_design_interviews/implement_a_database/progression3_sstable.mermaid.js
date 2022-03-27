9599688480080bb17fc321b24400288038265129
Counterfly <mark.loyzer@gmail.com>
2022-03-27 05:59:47 -0700

    Add sstable mermaid js diagram

---
+++ grokking_system_design_interviews/implement_a_database/progression3_sstable.mermaid.js
@@ -0,0 +1,24 @@
+erDiagram
+  SegmentDefintion {
+    string bytes
+    key value
+  }
+
+  As {
+    aardvark location
+    abbrevation location
+  }
+  Bs {
+    bable locatoin
+    back location
+  }
+  
+  Zs {
+    zafu location
+    zag location
+  }
+
+
+  As ||--|| Bs: preceeds
+  As ||--|| Bs: preceeds
+  Bs ||--|| Zs: "..."
